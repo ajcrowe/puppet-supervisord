dac16a643a961f6501febb4aae1f983d85c36175
Jeremy Kitchen <kitchen@kitchen.io>
2014-09-05 21:17:37 +0000

    supervisord::rpcinterface define

---
+++ manifests/rpcinterface.pp
@@ -0,0 +1,29 @@
+# Define: supervisord::rpcinterface
+#
+# This define creates an rpcinterface configuration file
+#
+# Documentation on parameters available at:
+# http://supervisord.org/configuration.html#rpcinterface-x-section-settings
+#
+define supervisord::rpcinterface (
+  $rpcinterface_factory,
+  $ensure                = present,
+  $retries               = undef,
+) {
+
+  include supervisord
+
+  # parameter validation
+  if $retries { validate_re($retries, '^\d+') }
+
+  $conf = "${supervisord::config_include}/rpcinterface_${name}.conf"
+
+  file { $conf:
+    ensure  => $ensure,
+    owner   => 'root',
+    mode    => '0644',
+    content => template('supervisord/conf/rpcinterface.erb'),
+    notify  => Class['supervisord::reload']
+  }
+
+}
