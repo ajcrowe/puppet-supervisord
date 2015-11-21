a67f04c660cfb1e958b691556c400d018b417b3c
Alex Crowe <alex.j.crowe@googlemail.com>
2015-11-21 15:14:08 +0000

    fixed indentation

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,10 +10,10 @@ class supervisord::params {
       $install_init      = true
       case $::operatingsystem {
         'Amazon':{
-          $executable_path   = '/usr/local/bin'
+          $executable_path = '/usr/local/bin'
         }
         default: {
-          $executable_path   = '/usr/bin'
+          $executable_path = '/usr/bin'
         }
       }
     }
@@ -38,53 +38,53 @@ class supervisord::params {
   }
 
   # default supervisord params
-  $package_ensure       = 'installed'
-  $package_provider     = 'pip'
+  $package_ensure          = 'installed'
+  $package_provider        = 'pip'
   $package_install_options = undef
-  $service_manage       = true
-  $service_ensure       = 'running'
-  $service_enable       = true
-  $service_name         = 'supervisord'
-  $service_restart      = undef
-  $package_name         = 'supervisor'
-  $executable           = "${executable_path}/supervisord"
-  $executable_ctl       = "${executable_path}/supervisorctl"
+  $service_manage          = true
+  $service_ensure          = 'running'
+  $service_enable          = true
+  $service_name            = 'supervisord'
+  $service_restart         = undef
+  $package_name            = 'supervisor'
+  $executable              = "${executable_path}/supervisord"
+  $executable_ctl          = "${executable_path}/supervisorctl"
 
-  $scl_enabled          = false
-  $scl_script           = '/opt/rh/python27/enable'
+  $scl_enabled             = false
+  $scl_script              = '/opt/rh/python27/enable'
 
-  $run_path             = '/var/run'
-  $pid_file             = 'supervisord.pid'
-  $log_path             = '/var/log/supervisor'
-  $log_file             = 'supervisord.log'
-  $logfile_maxbytes     = '50MB'
-  $logfile_backups      = '10'
-  $log_level            = 'info'
-  $nodaemon             = false
-  $minfds               = '1024'
-  $minprocs             = '200'
-  $umask                = '022'
-  $manage_config        = true
-  $config_include       = '/etc/supervisor.d'
-  $config_file          = '/etc/supervisord.conf'
-  $setuptools_url       = 'https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py'
+  $run_path                = '/var/run'
+  $pid_file                = 'supervisord.pid'
+  $log_path                = '/var/log/supervisor'
+  $log_file                = 'supervisord.log'
+  $logfile_maxbytes        = '50MB'
+  $logfile_backups         = '10'
+  $log_level               = 'info'
+  $nodaemon                = false
+  $minfds                  = '1024'
+  $minprocs                = '200'
+  $umask                   = '022'
+  $manage_config           = true
+  $config_include          = '/etc/supervisor.d'
+  $config_file             = '/etc/supervisord.conf'
+  $setuptools_url          = 'https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py'
 
-  $ctl_socket           = 'unix'
+  $ctl_socket              = 'unix'
 
-  $unix_socket          = true
-  $unix_socket_file     = 'supervisor.sock'
-  $unix_socket_mode     = '0700'
-  $unix_socket_owner    = 'nobody'
-  $unix_auth            = false
-  $unix_username        = undef
-  $unix_password        = undef
+  $unix_socket             = true
+  $unix_socket_file        = 'supervisor.sock'
+  $unix_socket_mode        = '0700'
+  $unix_socket_owner       = 'nobody'
+  $unix_auth               = false
+  $unix_username           = undef
+  $unix_password           = undef
 
-  $inet_server          = false
-  $inet_server_hostname = '127.0.0.1'
-  $inet_server_port     = '9001'
-  $inet_auth            = false
-  $inet_username        = undef
-  $inet_password        = undef
+  $inet_server             = false
+  $inet_server_hostname    = '127.0.0.1'
+  $inet_server_port        = '9001'
+  $inet_auth               = false
+  $inet_username           = undef
+  $inet_password           = undef
 
-  $init_template        = "supervisord/init/${::osfamily}/defaults.erb"
+  $init_template           = "supervisord/init/${::osfamily}/defaults.erb"
 }
