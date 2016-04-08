0a18a551f33087a9148a990c8f0486d66efdf3a7
brutus333 <virgil.chereches@gmx.net>
2016-04-08 16:02:12 +0300

    Adding support for proxy
    In cases where node does not have direct internet connection, a proxy is required. Since older version of pip does not honor proxy configuration setting the simplest way is to use exec with environment.
    We tried also setting the proxy in puppet.conf file but it has the unwanted effect of using the proxy for puppet agent-master communication.

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,9 +3,20 @@
 # Installs supervisor package (defaults to using pip)
 #
 class supervisord::install inherits supervisord {
-  package { $supervisord::package_name:
-    ensure          => $supervisord::package_ensure,
-    provider        => $supervisord::package_provider,
-    install_options => $supervisord::package_install_options,
+  if $::supervisord::pip_proxy {
+    exec { "pip-install-supervisor":
+      user        => root,
+      path        => ["/usr/bin","/bin"],
+      environment => [ "http_proxy=${supervisord::pip_proxy}", "https_proxy=${supervisord::pip_proxy}" ],
+      command     => "pip install ${supervisord::package_name}",
+      unless      => "which supervisorctl",
+    }
+  }
+  else {
+    package { $supervisord::package_name:
+      ensure          => $supervisord::package_ensure,
+      provider        => $supervisord::package_provider,
+      install_options => $supervisord::package_install_options,
+    }
   }
 }
