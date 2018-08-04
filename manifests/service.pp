eba48a6e4a252c3fc73bcefd915a6319ccf7fee6
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2018-08-04 05:32:15 -0500

    systemd LimitNOFILE support (#121)
    * systemd LimitNOFILE support
    * refresh systemd unit

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,6 +4,14 @@
 #
 class supervisord::service inherits supervisord  {
   if $::supervisord::service_manage {
+    if $::supervisord::init_type == 'systemd' {
+      exec { 'refresh_supervisord_unit':
+        command     => '/usr/bin/env systemctl daemon-reload',
+        refreshonly => true,
+        before      => Service[$::supervisord::service_name],
+      }
+    }
+
     service { $::supervisord::service_name:
       ensure     => $::supervisord::service_ensure,
       enable     => $::supervisord::service_enable,
