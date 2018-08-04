0c0f3cfad60395e21bebefedbc182bd6f6f74565
wardhus <ward.hus@gmail.com>
2018-08-04 12:40:45 +0200

    Adding support for multiple processes (#109)
    This fixes #103

--- manifests/program.pp
+++ manifests/program.pp
@@ -131,23 +131,30 @@ define supervisord::program(
     }
   }
 
+  if ($numprocs != 1 ) {
+    $pname = "${name}:*"
+  }
+  else {
+    $pname = $name
+  }
+
   case $ensure_process {
     'stopped': {
       supervisord::supervisorctl { "stop_${name}":
         command => 'stop',
-        process => $name
+        process => $pname
       }
     }
     'removed': {
       supervisord::supervisorctl { "remove_${name}":
         command => 'remove',
-        process => $name
+        process => $pname
       }
     }
     'running': {
       supervisord::supervisorctl { "start_${name}":
         command => 'start',
-        process => $name,
+        process => $pname,
         unless  => 'running'
       }
     }
